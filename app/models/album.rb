18a8294bb395559982143a173b0efe59b4eac1bb
ccloes <ccloes@gmail.com>
2017-03-05 23:10:27 -0800

    Updated to check dependencies

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,4 @@
 class Album < ApplicationRecord
   belongs_to :user
-  has_many :photos
+  has_many :photos, :dependent => :restrict_with_error
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-  has_many :albums
+  has_many :albums, :dependent => :restrict_with_error
 end
