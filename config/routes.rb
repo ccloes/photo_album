9df7b3b7cdeaafbad9c2f049ed7bde462d82dbfb
ccloes <ccloes@gmail.com>
2017-03-06 09:23:57 -0800

    Updated to add navigation

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :users
   root "pages#show", page: "home"
   get "/pages/:page" => "pages#show"
-  get    '/init',   to: 'users#init'
+  get    "/init",   to: "users#init"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
